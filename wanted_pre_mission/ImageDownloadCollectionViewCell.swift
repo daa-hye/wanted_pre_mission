b7547ac6670d04873edad2a35a60e023241b4e45
dahye <bbngsk@gmail.com>
2023-02-23 22:08:56 +0900

    CustomCell 생성

---
+++ wanted_pre_mission/ImageDownloadCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImageDownloadCollectionViewCell.swift
+//  wanted_pre_mission
+//
+//  Created by 박다혜 on 2023/02/23.
+//
+
+import UIKit
+
+class ImageDownloadCollectionViewCell: UICollectionViewCell {
+    
+}
